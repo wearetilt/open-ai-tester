1b063bc8b9151a97b546bd3a19c6eedc557088cb
joshyhud <joshhudsondev@gmail.com>
2023-02-06 14:10:47 +0000

    updated text input to generate images instead

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ const getResponseFromOpenAI = async () => {
         className={styles.promptInput}
         placeholder="Enter a prompt"
         onChange={(e) => setPrompt(e.target.value)}
-        row={5}
+        rows={5}
         cols={50}
       />
       <button className={styles.button} onClick={getResponseFromOpenAI}>
