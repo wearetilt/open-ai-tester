5429098e2b64ef2b25181274a8248cfa0e5f50b7
joshyhud <joshhudsondev@gmail.com>
2023-03-02 08:43:24 +0000

    testing out gpt3.5 turbo

--- src/pages/api/openai.tsx
+++ src/pages/api/openai.tsx
@@ -14,9 +14,10 @@ export default async (req: any, res: any) => {
   if (req.body.prompt !== undefined) {
     const response = await openai.createImage({
       // prompt: "photo realistic image of a pixar style cat in a field of poppies and a dog in the distance",
+      model: "gpt-3.5-turbo",
       prompt: `${req.body.prompt}`,
       n: 1,
-      size: "512x512",
+      size: "1024x1024",
     });
 
     res.status(200).json({ image: `${response.data.data[0].url}` });
